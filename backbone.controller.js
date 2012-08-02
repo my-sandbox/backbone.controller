a22cc9e5748532e058db34b0f11b4272b4e69f26
k33g_org <ph.charriere@gmail.com>
2012-08-02 10:50:28 +0200

    cleaner version

--- backbone.controller.js
+++ backbone.controller.js
@@ -2,4 +2,6 @@
 	Backbone Controllers "à la" Playframework by K33G
 =================================================================*/
 
-Backbone.Controller = Backbone.View.extend();
+//Backbone.Controller = Backbone.View.extend();
+Backbone.Controller = function() {};
+Backbone.Controller.extend = Backbone.View.extend();
